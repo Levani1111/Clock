870a97f222e68fb1a967d37aa31e2aca36d53875
Levani Papashvili <papashvililevani@gmail.com>
2020-07-08 11:24:28 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
